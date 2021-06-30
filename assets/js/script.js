a1b4b7e349dbe45aaccedf8479d2cf4aac23fba9
Alena Rehberger <alenarehberger92@gmail.com>
2021-06-29 23:39:38 -0500

    Made changes to javascript file

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,43 +4,21 @@ var generateBtn = document.querySelector("#generate");
 // Possible password values
 var characters = '1234567890abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz!"@#$%^&*()_-+=[]{};.,<>\|~';
 var passwordLength = '';
-var password = "";
+var password = '';
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  passwordText.value = password;
 }
 
 function generatePassword () {
-  prompt ("Enter in a desired number of characters.");
-
-    if (passwordLength != null) {
-      document.getElementById("password").value = password;
-    }
-
-    else if (passwordLength < 8) {
-      promt ("Password length must be at least 8 characters.");
-    }
-
-    else function charactersConfirmation() {
-      if (confirm("Click OK to confirm including special characters in your password.")) {
-        txt = "You pressed OK!";
-      } else {
-        txt = "You pressed Cancel!";
-      }
-      document.getElementById("password").value = password;
-    }
-
     for (var i = 0; i < passwordLength; i++) {
       var randomNumber = Math.floor(Math.random() * characters.length);
-      password += characters.substring(randomNumber, randomNumber+1);
+      password += characters.substring(randomNumber, randomNumber + 1);
   }
-
-} //end generate password definition
-
-
+    passwordText.value = password;
+} 
 
 document.getElementById("password").value = password;
 
@@ -49,3 +27,28 @@ document.getElementById("password").value = password;
 generateBtn.addEventListener("click", generatePassword);
 
 //alert ("Sorry, your password has to be no longer than 128 characteristics")
+function passwordLength () { 
+  if (passworLength = false) {
+    alert ("Password length must be provided as a number");
+  }
+  else if (passwordLength < 8) {
+    alert ("Password length must be at least 8 characters.");
+  }
+  else if (passwordLength > 129) {
+    alert ("Password length must be less than 129 characters.");
+  }
+  else {
+    generatePassword();
+  }
+}
+
+function charactersConfirmation () { 
+  if (confirm ("Click OK to confirm including special characters in your password.")) {
+    txt = "You pressed OK!"
+  } 
+  else {
+    txt = "You pressed Cancel!"
+  }
+  document.getElementById("password").value = password;
+  }
+}
