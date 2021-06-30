4860ffba12bf6637abfff79d413e325769417652
Alena Rehberger <alenarehberger92@gmail.com>
2021-06-30 11:03:18 -0500

    Added variables to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,40 +2,43 @@
 var generateBtn = document.querySelector("#generate");
 
 // Possible password values
-var characters = '1234567890abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz!"@#$%^&*()_-+=[]{};.,<>\|~';
-var passwordLength = '';
-var password = '';
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var symbols = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\", ^ ", "_", "`", "{", "|", "}", "~"];
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-}
 
+var passwordLength;
+var passwordNumbers;
+var passwordLowerCase;
+var passwordUpperCase;
+var passwordSymbols;
+
+
+//for loop to verify the length of the password
 function generatePassword () {
     for (var i = 0; i < passwordLength; i++) {
       var randomNumber = Math.floor(Math.random() * characters.length);
       password += characters.substring(randomNumber, randomNumber + 1);
-  }
-    passwordText.value = password;
+
+      return;
 } 
 
-document.getElementById("password").value = password;
 
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", generatePassword);
-
-//alert ("Sorry, your password has to be no longer than 128 characteristics")
+//alerts ("Sorry, your password has to be longer than 8 and no longer than 128 characteristics")
 function passwordLength () { 
-  if (passworLength = false) {
-    alert ("Password length must be provided as a number");
+  if (isNaN(passwordLength) === true) {
+    alert("Password length must be provided as a number.");
+    return 
   }
+      
   else if (passwordLength < 8) {
     alert ("Password length must be at least 8 characters.");
+    return;
   }
-  else if (passwordLength > 129) {
+  else if (passwordLength > 128) {
     alert ("Password length must be less than 129 characters.");
+    return; 
   }
   else {
     generatePassword();
@@ -49,6 +52,17 @@ function charactersConfirmation () {
   else {
     txt = "You pressed Cancel!"
   }
-  document.getElementById("password").value = password;
+  document.getElementById("password").value = generatePassword;
   }
 }
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", generatePassword);
