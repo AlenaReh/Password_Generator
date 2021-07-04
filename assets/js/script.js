bd826d36ddd6fa426a82e129ebca25517e126bae
Alena Rehberger <alenarehberger92@gmail.com>
2021-07-03 23:35:23 -0500

    Made alerts more polite, removed console.logs, and deleted extra spaces between symbols

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ var password = {
   numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
   lowerCase: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
   upperCase: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
-  symbols: ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", " < ", "=", " > ", " ? ", "@", "[", "]", "\", ^ ", "_", "`", "{", "|", "}", "~"],
+  symbols: ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", " < ", "=", " > ", "?", "@", "[", "]", "`", "|", "~"],
 }
 
 //////////////////////// FUNCTIONS 
@@ -16,7 +16,6 @@ var password = {
 // Generating a password function with empty arrays.
 function generatePassword() { 
   var userPasswordParameters = userInput();
-  // console.log("userPasswordParameters", userPasswordParameters);
  return userPasswordParameters; 
 }
 
@@ -24,14 +23,13 @@ function generatePassword() {
 function randomChar(array) {
     var i = Math.floor(Math.random() * array.length);
     var element = array[i];
-    // console.log("random element" , element);
     return element;
 } 
 
 //Main logic to return the pasasword based on user inputs 
 function userInput () { 
 
-    var passwordLength = parseInt(prompt ("Please provide a desired number of characters for generating a password."));
+    var passwordLength = parseInt(prompt ("Please provide a desired number of characters to generate the password."));
     // User's possible choices
     var passwordNumbers;
     var passwordLowerCase;
@@ -46,28 +44,28 @@ function userInput () {
   
     //Initial alerts
     if (isNaN(passwordLength) === true) {
-      alert("Password length must be provided as a number. Try again!");
-      return ;
+      alert("Sorry, the password length must be provided as a number. Please try again!");
+      return
       
     } 
     console.log(typeof passwordLength);
     if( typeof passwordLength === 'string') {
-      alert ("Password length must be provided as a number. Try again!");
-      return ;
+      alert ("Sorry, the password length must be provided as a number. Please try again!");
+      return
     }
     if (passwordLength <= 8) {
-      alert ("Password length must be at least 8 characters.");
-      return ; 
+      alert ("Sorry, the password length must be at least 8 characters. Please try again!");
+      return
     }
     if (passwordLength >= 128) {
-      alert ("Password length must be less than 128 characters.");
-      return ;
+      alert ("Sorry, the password length must be less than 128 characters. Please try again!");
+      return
     }
 
      // Confirms
-     passwordNumbers = confirm ("Click OK to include numbers in your password.");
-     passwordLowerCase = confirm ("Click OK to include lower case letters in your password.");
      passwordUpperCase = confirm ("Click OK to include upper case letters in your password.");
+     passwordLowerCase = confirm ("Click OK to include lower case letters in your password.");
+     passwordNumbers = confirm ("Click OK to include numbers in your password.");
      passwordSymbols = confirm ("Click OK to include special characters in your password.");
 
     //  Alert for when a user doesn't select neither of the parameters for the password
@@ -96,7 +94,6 @@ function userInput () {
         charactersInc = charactersInc.concat(password.lowerCase);
         userCharacters.push(randomChar(password.lowerCase));
       }
-      // console.log(charactersInc); 
 
       // Loop to match the password with the chosen length.
       for (var i = 0; i < passwordLength; i++) {
